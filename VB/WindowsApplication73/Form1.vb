a5bc44f4481e7092a7352f2d628ee33e2de997c8
DevExpressExampleBot <fakeEmail>
2023-09-05 13:19:22 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication73/Form1.vb
+++ VB/WindowsApplication73/Form1.vb
@@ -1,36 +1,30 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 
 Namespace ImmediateInitRow
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub gridView1_InitNewRow(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.InitNewRowEventArgs) Handles gridView1.InitNewRow
+    Public Partial Class Form1
+        Inherits Form
 
-			 Dim view As GridView = TryCast(sender, GridView)
-			 view.SetRowCellValue(e.RowHandle, view.Columns("Name"), "Noname")
-			 view.SetRowCellValue(e.RowHandle, view.Columns("Date"), DateTime.Today)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub gridView1_InitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
+            Dim view As GridView = TryCast(sender, GridView)
+            view.SetRowCellValue(e.RowHandle, view.Columns("Name"), "Noname")
+            view.SetRowCellValue(e.RowHandle, view.Columns("Date"), Date.Today)
+        End Sub
 
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+        End Sub
 
-		Private Sub gridView1_ShownEditor(ByVal sender As Object, ByVal e As EventArgs) Handles gridView1.ShownEditor
-			Dim view As GridView = TryCast(sender, GridView)
-			If view.IsNewItemRow(view.FocusedRowHandle) AndAlso view.FocusedColumn.FieldName <> "ID" Then
-				view.ActiveEditor.IsModified = True
-			End If
-		End Sub
-	End Class
+        Private Sub gridView1_ShownEditor(ByVal sender As Object, ByVal e As EventArgs)
+            Dim view As GridView = TryCast(sender, GridView)
+            If view.IsNewItemRow(view.FocusedRowHandle) AndAlso Not Equals(view.FocusedColumn.FieldName, "ID") Then view.ActiveEditor.IsModified = True
+        End Sub
+    End Class
 End Namespace
